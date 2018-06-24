a73214a2068c4520f907597fd135050a1c22e3c8
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-24 14:12:03 -0500

    Add project files.

---
+++ GIG-HUB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GIG_HUB.MvcApplication" Language="C#" %>
---
+++ GIG-HUB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GIG_HUB.Startup))]
+namespace GIG_HUB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
