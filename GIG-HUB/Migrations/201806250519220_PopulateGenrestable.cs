8d15961a65c4ca95166afe6344516f3a0bbe9bb1
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-25 00:23:11 -0500

    Create empty migration for populating Genre

---
+++ GIG-HUB/Migrations/201806250519220_PopulateGenrestable.cs
@@ -0,0 +1,16 @@
+namespace GIG_HUB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenrestable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
