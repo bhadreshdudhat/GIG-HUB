7423e3b762d7565203dc2facb55e1004ebdf375f
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-25 00:41:27 -0500

    Revert "Populate Genres table with SQL script"
    This reverts commit befad345f2b7807e77dc00d202c49803120e3fb7.

--- GIG-HUB/Migrations/201806250519220_PopulateGenrestable.cs
+++ GIG-HUB/Migrations/201806250519220_PopulateGenrestable.cs
@@ -7,20 +7,10 @@ namespace GIG_HUB.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Genres (Id,Name) VALUES (1, 'Jazz')");
-            Sql("INSERT INTO Genres (Id,Name) VALUES (2, 'Blue')");
-            Sql("INSERT INTO Genres (Id,Name) VALUES (3, 'Rock')");
-            Sql("INSERT INTO Genres (Id,Name) VALUES (4, 'Country')");
         }
-
-       //in code-first data flow it is possible to take database to any versions
-       //on update-datbase by-default it goes to last migration
-       //so when modifying the up method it's good practice to modify Down() method
-       //to put database in the right state
-       //UP() is opposite of Down()
+        
         public override void Down()
         {
-            Sql("DELETE FROM Genres WHERE Id IN (1,2,3,4)");
         }
     }
 }
