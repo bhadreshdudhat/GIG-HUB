51b1257f827c1a3af7a1697bf964607f2b5451c0
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-26 01:17:24 -0500

    Add Gig to database

--- GIG-HUB/Controllers/GigsController.cs
+++ GIG-HUB/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GIG_HUB.Models;
 using GIG_HUB.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -19,6 +21,26 @@ namespace GIG_HUB.Controllers
         {
             var viewModel = new GigsFormViewModel() { Genres = _context.Genres.ToList() };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigsFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
