d8250bb4c2189d6dd327aa3ad9141bb350cf36f6
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-27 18:21:18 -0500

    Add serverside Custom validations (for Date and Time)

---
+++ GIG-HUB/ViewModels/FutureDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GIG_HUB.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            bool isValid = System.DateTime.TryParseExact(Convert.ToString(value),
+                                            "d MMM yyyy",
+                                            CultureInfo.CurrentCulture,
+                                            DateTimeStyles.None,
+                                            out DateTime dateTime
+                                         );
+
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
