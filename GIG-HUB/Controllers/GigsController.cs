d5d40d44770fea80fcd4e25a770dd3130788f639
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-06-27 17:34:33 -0500

    Add Requied Validations. (Add Data-annotations in ViewModel and  validation Message field in View )

--- GIG-HUB/Controllers/GigsController.cs
+++ GIG-HUB/Controllers/GigsController.cs
@@ -26,10 +26,15 @@ namespace GIG_HUB.Controllers
         [HttpPost]
         public ActionResult Create(GigsFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();//viewmodel coming from Create view has it's Genres=null 
+                return View("Create", viewModel);
+            }
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
--- GIG-HUB/ViewModels/GigsFormViewModel.cs
+++ GIG-HUB/ViewModels/GigsFormViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using GIG_HUB.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GIG_HUB.ViewModels
 {
     public class GigsFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+
+        //reflection will check all fields & properties so, DateTime will give FormateException 
+        public DateTime GetDateTime()
         {
-            get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); }
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
 
     }
