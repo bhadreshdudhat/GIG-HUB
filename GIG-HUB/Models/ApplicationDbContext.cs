311c17c380107c21e273ba06cb8cde831d57b242
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-07-03 11:30:31 -0500

    Add Attendance class (Disable Cascade On Delete)

--- GIG-HUB/Models/ApplicationDbContext.cs
+++ GIG-HUB/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace GIG_HUB.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,17 @@ namespace GIG_HUB.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        // overriding default convention
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
