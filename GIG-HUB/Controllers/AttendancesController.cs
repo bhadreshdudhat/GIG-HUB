779e1cd421063b4c1a68c7b1384ac9ce98d42e19
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-07-03 18:52:44 -0500

    Add Button for Attendance

--- GIG-HUB/Controllers/AttendancesController.cs
+++ GIG-HUB/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using GIG_HUB.Models;
+﻿using GIG_HUB.Dtos;
+using GIG_HUB.Models;
 using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Http;
@@ -16,17 +17,17 @@ namespace GIG_HUB.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int gigId)
+        public IHttpActionResult Attend(AttendanceDto dto)
         {
             var userId = User.Identity.GetUserId();
 
             //if attandance is already present
-            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
                 return BadRequest("Attendance is already exists.");
 
             var attendance = new Attendance
             {
-                GigId = gigId,
+                GigId = dto.GigId,
                 AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
