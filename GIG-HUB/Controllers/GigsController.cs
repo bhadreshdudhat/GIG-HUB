874d9629dac4e72582a1822c64f36cec14642b80
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-07-03 23:54:40 -0500

    Add edit and Update Functionality

--- GIG-HUB/Controllers/GigsController.cs
+++ GIG-HUB/Controllers/GigsController.cs
@@ -53,8 +53,30 @@ namespace GIG_HUB.Controllers
         [Authorize]
         public ActionResult Create()
         {
-            var viewModel = new GigsFormViewModel() { Genres = _context.Genres.ToList() };
-            return View(viewModel);
+            var viewModel = new GigsFormViewModel()
+            {
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig"
+            };
+            return View("GigForm", viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            var viewModel = new GigsFormViewModel()
+            {
+                Genres = _context.Genres.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue,
+                Heading = "Edit a Gig",
+                Id = gig.Id
+            };
+            return View("GigForm", viewModel);
         }
 
         [Authorize]
@@ -65,7 +87,7 @@ namespace GIG_HUB.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genres.ToList();//viewmodel coming from Create view has it's Genres=null 
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
             var gig = new Gig
             {
@@ -75,6 +97,29 @@ namespace GIG_HUB.Controllers
                 Venue = viewModel.Venue
             };
             _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigsFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();//viewmodel coming from Create view has it's Genres=null 
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
             _context.SaveChanges();
 
             return RedirectToAction("Mine", "Gigs");
--- GIG-HUB/ViewModels/GigsFormViewModel.cs
+++ GIG-HUB/ViewModels/GigsFormViewModel.cs
@@ -7,6 +7,8 @@ namespace GIG_HUB.ViewModels
 {
     public class GigsFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -22,6 +24,17 @@ namespace GIG_HUB.ViewModels
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                return (Id != 0) ? "Update" : "Create";
+            }
+
+        }
 
         //reflection will check all fields & properties so, DateTime will give FormateException 
         public DateTime GetDateTime()
