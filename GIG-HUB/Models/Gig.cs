74823ee602ed930881ac0105d6f9f72480040c2e
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2018-07-05 11:11:00 -0500

    Add IsCanceled property to Gig

--- GIG-HUB/Models/Gig.cs
+++ GIG-HUB/Models/Gig.cs
@@ -7,6 +7,8 @@ namespace GIG_HUB.Models
     {
         public int Id { get; set; }
 
+        public bool IsCanceled { get; set; }
+
         public ApplicationUser Artist { get; set; }
 
         [Required]
